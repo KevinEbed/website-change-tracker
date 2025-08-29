8ea1abc97d5fef9903c1f3c8f3161df0b22bd544
KevinEbed <133675857+KevinEbed@users.noreply.github.com>
2025-08-29 09:52:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -209,4 +209,4 @@ def delete_url(url_id):
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
